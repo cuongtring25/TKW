93544031cfeb2928b6bbfbb68a3babcc365ac52f
cuongtring25 <cuongtrinh574@gmail.com>
2024-11-06 00:11:11 +0700

    update lan cuoi

---
+++ js/about.js
@@ -0,0 +1,11 @@
+const contentSections = document.querySelectorAll(".info-section");
+window.addEventListener("scroll", () => {
+  contentSections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    if (sectionTop < window.innerHeight) {
+      section.classList.add("visible");
+    } else {
+      section.classList.remove("visible");
+    }
+  });
+});
